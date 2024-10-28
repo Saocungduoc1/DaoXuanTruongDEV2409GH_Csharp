81a055e67fbd5c7b81a140d12f71772ce8892e09
Saocungduoc1 <daotruongdaotrang@gmail.com>
2024-10-28 10:57:54 +0700

    c#

---
+++ Sesson29/KiemTra/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace KiemTra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Hashtable studentTable = new Hashtable(); //lưu sinh viên
+            int option;
+
+            do
+            {
+                Console.WriteLine("===============================");
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Thêm mới sinh viên.");
+                Console.WriteLine("2. Hiển thị danh sách sinh viên.");
+                Console.WriteLine("3. Tính điểm trung bình.");
+                Console.WriteLine("4. Thoát.");
+                Console.Write("Option: ");
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        InsertStudent(studentTable);
+                        break;
+                    case 2:
+                        DisplayStudents(studentTable);
+                        break;
+                    case 3:
+                        CalculateAvgMarks(studentTable);
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            } while (option != 4);
+        }
+
+        //hàm thêm sinh viên
+        static void InsertStudent(Hashtable studentTable)
+        {
+            Student student = new Student();
+            Console.Write("Mã sinh viên: ");
+            student.StudID = int.Parse(Console.ReadLine());
+
+            Console.Write("Tên sinh viên: ");
+            student.StudName = Console.ReadLine();
+
+            Console.Write("Giới tính: ");
+            student.StudGender = Console.ReadLine();
+
+            Console.Write("Tuổi: ");
+            student.StudAge = int.Parse(Console.ReadLine());
+
+            Console.Write("Lớp: ");
+            student.StudClass = Console.ReadLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Môn thứ {i + 1}: ");
+                student[i] = int.Parse(Console.ReadLine()) ;
+            }
+
+            student.CalAvg(); // tính điểm trung bình
+            studentTable.Add(student.StudID, student); 
+        }
+
+        static void DisplayStudents(Hashtable studentTable)
+        {
+            foreach (DictionaryEntry entry in studentTable)
+            {
+                Student student = (Student)entry.Value;
+                student.Print();
+            }
+        }
+
+        static void CalculateAvgMarks(Hashtable studentTable)
+        {
+            foreach (Student student in studentTable.Values)
+            {
+                Console.WriteLine("Mark: {0:N2}", student.StudAvgMark);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Sesson29/KiemTra/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiemTra
+{
+    public class Student : IStudent
+    {
+        public int StudID { get; set; }
+        public string StudName { get; set; }
+        public string StudGender { get; set; }
+        public int StudAge { get; set; }
+        public string StudClass { get; set;}
+        private float studAvgMark;
+        public float StudAvgMark
+        {
+            get { return studAvgMark; }
+        }
+        //MẢNG LƯU ĐIỂM
+        private int[] MarkList = new int[3];
+
+        //phương thức in thông tin
+        public void Print()
+        {
+            Console.WriteLine($"ID: {StudID}, Name: {StudName}, Gender: {StudGender}, Age: {StudAge}, Class: {StudClass}, Avg Mark: {StudAvgMark}");
+        }
+
+        //indexxer để truy cập và gán điểm chi mảng marklisst
+        public int this[int index]
+        {
+            get { return MarkList[index]; }
+            set { MarkList[index] = value;}
+    }
+
+        //phương thức tính điểm trung bình
+        public void CalAvg()
+        {
+            //kiểm tra xem mảng có rỗng hoặc chưa được khởi tạo đầy đủ
+            /* if (MarkList == null || MarkList.Length == 0 || MarkList.All(x => x == 0))
+             {
+                 Console.WriteLine("Error: Marklist is either empty or contains invalid values."); ;
+             }
+             else 
+             {
+                 //sử dụng Average() của LINQ để tính điểm trung bình nếu hợp lệ
+                 studAvgMark = (float)MarkList.Average();
+             }*/
+            studAvgMark = (MarkList[0] + MarkList[1] + MarkList[2]) / 3;
+        }
+    }
+}
